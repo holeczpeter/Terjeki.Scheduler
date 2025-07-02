e1c0636e4691f98d6b8b807f8411941e4ada74a2
holecz.peter85 <holecz.peter85@gmail.com>
2025-07-02 17:38:26 +0200

    add HealthController

---
+++ Terjeki.Scheduler.Api/Controllers/HealthController.cs
@@ -0,0 +1,10 @@
+﻿namespace Terjeki.Scheduler.Api.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { status = "Healthy", time = DateTime.UtcNow });
+    }
+}
